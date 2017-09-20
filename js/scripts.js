90e617579e8f6e9b10fc661086397fcb82f9af92
Joseph Escobedo <joe_escob1023@live.com>
2017-09-20 15:13:27 -0700

    add jquery library and scripts for making flash cards slide and toggle

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$('.clickable-1').click(function() {
+	$('.initially-hidden-1').slideToggle();
+	$('.initially-showing-1').slideToggle();
+});
+
+$('.clickable-2').click(function() {
+	$('.initially-hidden-2').slideToggle();
+	$('.initially-showing-2').slideToggle();
+});
+
+$('.clickable-3').click(function() {
+	$('.initially-hidden-3').slideToggle();
+	$('.initially-showing-3').slideToggle();
+});
+
+$('.clickable-4').click(function() {
+	$('.initially-hidden-4').slideToggle();
+	$('.initially-showing-4').slideToggle();
+});
+
+$('.clickable-5').click(function() {
+	$('.initially-hidden-5').slideToggle();
+	$('.initially-showing-5').slideToggle();
+});
+
+$('.clickable-6').click(function() {
+	$('.initially-hidden-6').slideToggle();
+	$('.initially-showing-6').slideToggle();
+});
+
+$('.clickable-7').click(function() {
+	$('.initially-hidden-7').slideToggle();
+	$('.initially-showing-7').slideToggle();
+});
+
+$('.clickable-8').click(function() {
+	$('.initially-hidden-8').slideToggle();
+	$('.initially-showing-8').slideToggle();
+});
+
+$('.clickable-9').click(function() {
+	$('.initially-hidden-9').slideToggle();
+	$('.initially-showing-9').slideToggle();
+});
